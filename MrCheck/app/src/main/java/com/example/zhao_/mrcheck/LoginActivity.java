97dd3a2acaf3f6613636b9659e3f67da27124434
nuomizai <769572294@qq.com>
2017-11-28 16:49:21 +0800

    注册界面

--- MrCheck/app/src/main/java/com/example/zhao_/mrcheck/LoginActivity.java
+++ MrCheck/app/src/main/java/com/example/zhao_/mrcheck/LoginActivity.java
@@ -1,12 +1,34 @@
 package com.example.zhao_.mrcheck;
 
+import android.support.v4.media.MediaMetadataCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+    private EditText editUserName;
+    private EditText editPassWord;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+        Button button1 = (Button) findViewById(R.id.loginButton);
+        editUserName=(EditText)findViewById(R.id.edit_username);
+        editPassWord=(EditText)findViewById(R.id.edit_password);
+        button1.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v){
+        switch(v.getId()){
+            case R.id.loginButton:
+                String input_username=editUserName.getText().toString();
+                String input_password=editPassWord.getText().toString();
+                break;
+            default:
+                break;
         }
+    }
     }
