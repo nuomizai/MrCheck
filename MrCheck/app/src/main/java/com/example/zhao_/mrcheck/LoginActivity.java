6652824d3988ea30051895df2c15fd0ee17a9e2c
nuomizai <769572294@qq.com>
2017-11-28 15:20:49 +0800

    主页面修改

--- MrCheck/app/src/main/java/com/example/zhao_/mrcheck/LoginActivity.java
+++ MrCheck/app/src/main/java/com/example/zhao_/mrcheck/LoginActivity.java
@@ -3,11 +3,10 @@ package com.example.zhao_.mrcheck;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
+public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_layout);
+        }
     }
-}
