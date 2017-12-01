26a6bde286513480d4913afea05406743a4d851c
nuomizai <769572294@qq.com>
2017-12-01 15:35:31 +0800

    注册界面

--- MrCheck/app/src/main/java/com/example/zhao_/mrcheck/LoginActivity.java
+++ MrCheck/app/src/main/java/com/example/zhao_/mrcheck/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.zhao_.mrcheck;
 
+import android.app.DownloadManager;
 import android.support.v4.media.MediaMetadataCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,17 +9,22 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
     private EditText editUserName;
     private EditText editPassWord;
+    private Button LButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
-        Button button1 = (Button) findViewById(R.id.loginButton);
+        LButton = (Button) findViewById(R.id.loginButton);
         editUserName=(EditText)findViewById(R.id.edit_username);
         editPassWord=(EditText)findViewById(R.id.edit_password);
-        button1.setOnClickListener(this);
+        LButton.setOnClickListener(this);
     }
     @Override
     public void onClick(View v){
@@ -30,5 +36,25 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             default:
                 break;
         }
+        if(v.getId() == R.id.loginButton){
+            sendRequestWithOkHttp();
+        }
     }
+    private void sendRequestWithOkHttp(){
+        new Thread(new Runnable(){
+            @Override
+            public void run() {
+                try{
+                    OkHttpClient client = new OkHttpClient();
+                    Request request=new Request.Builder()
+                            .url("http://www.baidu.com")
+                            .build();
+                    Response response=client.newCall(request).execute();
+                    String responseData=response.body().string();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
+}
--- MrCheck/app/src/main/java/com/example/zhao_/mrcheck/RegistActivity.java
+++ MrCheck/app/src/main/java/com/example/zhao_/mrcheck/RegistActivity.java
@@ -2,12 +2,66 @@ package com.example.zhao_.mrcheck;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.TextView;
+import android.util.Log;
+import android.os.Message;
+
+import okhttp3.*;
+import java.io.IOException;
 
 public class RegistActivity extends AppCompatActivity {
-
+    private Button reg;
+    private EditText uname,pwd;
+    private String name1,pwd1;
+    private TextView text1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regist);
+        uname=(EditText)findViewById(R.id.username);
+        pwd=(EditText)findViewById(R.id.password);
+        reg=(Button)findViewById(R.id.registButton);
+        reg.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                name1=uname.getText().toString().trim();
+                pwd1=pwd.getText().toString().trim();
+                postRequest(name1,pwd1);
+
+            }
+        });
+    }
+
+    private void postRequest(String name,String pwd){
+        FormBody formBody= new FormBody.Builder()
+                .add("uname",name)
+                .add("pwd",pwd)
+                .add("method0","okhttpreg")
+                .build();
+        final Request request=new Request.Builder()
+                .url("http://111.230.237.110/Signin.php")
+                .post(formBody)
+                .build();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Response response=null;
+                try{
+                    OkHttpClient client= new OkHttpClient();
+                    if(response.isSuccessful()){
+
+                    }
+
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+
     }
 }
