8ae48b48469b48ad62baadb0cbc81a3ca4be79cb
nuomizai <769572294@qq.com>
2017-12-02 19:32:23 +0800

    与后台数据库链接

--- MrCheck/app/src/main/java/com/example/zhao_/mrcheck/RegistActivity.java
+++ MrCheck/app/src/main/java/com/example/zhao_/mrcheck/RegistActivity.java
@@ -9,59 +9,85 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.util.Log;
 import android.os.Message;
+import org.json.*;
 
 import okhttp3.*;
 import java.io.IOException;
 
 public class RegistActivity extends AppCompatActivity {
     private Button reg;
-    private EditText uname,pwd;
-    private String name1,pwd1;
+    private EditText uname,pwd,intro;
+    private String name1,pwd1,intro1;
     private TextView text1;
+    private Handler mHandler=new Handler(){
+        @Override
+        public void handleMessage(Message msg){
+            if(msg.what==1){
+                String qq=(String)msg.obj;
+                Log.i("RegistActivity",qq);
+                text1.setText(qq);
+            }
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regist);
+
         uname=(EditText)findViewById(R.id.username);
         pwd=(EditText)findViewById(R.id.password);
+        intro=(EditText)findViewById(R.id.introduction);
+        text1=(TextView)findViewById(R.id.response);
         reg=(Button)findViewById(R.id.registButton);
+
         reg.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
-                name1=uname.getText().toString().trim();
-                pwd1=pwd.getText().toString().trim();
-                postRequest(name1,pwd1);
-
+            public void onClick(View v){
+                    name1 = uname.getText().toString().trim();
+                    pwd1 = pwd.getText().toString().trim();
+                    intro1 = intro.getText().toString().trim();
+                    Log.d("RegistActivity", "name" + name1);
+                    Log.d("RegistActivity", "password" + pwd1);
+                    Log.d("RegistActivity", "introduction" + intro1);
+                    postRequest();
             }
         });
     }
 
-    private void postRequest(String name,String pwd){
-        FormBody formBody= new FormBody.Builder()
-                .add("uname",name)
-                .add("pwd",pwd)
-                .add("method0","okhttpreg")
-                .build();
-        final Request request=new Request.Builder()
-                .url("http://111.230.237.110/Signin.php")
-                .post(formBody)
-                .build();
+    private void postRequest(){
         new Thread(new Runnable() {
             @Override
             public void run() {
-                Response response=null;
+                RequestBody formBody= new FormBody.Builder()
+                        .add("uname",name1)
+                        .add("pwd",pwd1)
+                        .add("introduction",intro1)
+                        .build();
+                final Request request=new Request.Builder()
+                        .url("http://111.230.237.110/Signin.php")
+                        .post(formBody)
+                        .build();
                 try{
                     OkHttpClient client= new OkHttpClient();
-                    if(response.isSuccessful()){
-
-                    }
+                    Response response=client.newCall(request).execute();
+                    String responseData=response.body().string();
+                    parseJSONWithJSONObject(responseData);
 
                 }catch(Exception e){
                     e.printStackTrace();
                 }
             }
         }).start();
-
-
+    }
+    private void parseJSONWithJSONObject(String jsonData){
+        try{
+            JSONArray jsonArray=new JSONArray(jsonData);
+            JSONObject jsonObject=jsonArray.getJSONObject(0);
+            String msg=jsonObject.getString("msg");
+            Log.d("RegistActivity","msg is"+msg);
+            
+            }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
